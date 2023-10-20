580c2ef5764b91978c949088fc211b1379d3d668
GouthamJM <goutham@designstring.com>
2023-10-20 15:25:27 +0530

    challenge create

--- ui_components/challenges/ChallengeList.jsx
+++ ui_components/challenges/ChallengeList.jsx
@@ -1,10 +1,28 @@
+import { useRouter } from "next/router";
 import { ChallengeListItem } from ".";
+import { Button } from "../shared";
 
 export default function ChallengeList({ challengesDetail }) {
-    console.log(challengesDetail, "challengesDetail");
+    const router = useRouter();
+    const navigate = router.push;
+
     return (
         <div className="container mx-auto pt-6">
-            <p className="paragraph_bold text-black mb-3">Challenge</p>
+            <div className="flex justify-between pb-4">
+                <div className="col-span-6">
+                    <p className="paragraph_bold text-black mb-3">Challenge</p>
+                </div>
+                <div className="col-span-6">
+                    <Button
+                        variant={"primary"}
+                        onClick={() => navigate("create-challenge")}
+                        className={"h-6 text-sm bg-green2"}
+                    >
+                        + Create Challenge
+                    </Button>
+                </div>
+            </div>
+
             <div className="grid xl:grid-cols-1 grid-cols-1 gap-3">
                 {challengesDetail?.activeChallenges?.list?.map((challenge) => (
                     <ChallengeListItem
