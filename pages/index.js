21126624eccde77ab81ff900fd9506f834a92e0a
VaithiDs <vaithiyalingam@designstring.com>
2023-10-20 15:48:28 +0530

    Added bottom nav for challenge page

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,4 @@ import PageLayoutHoc from "@/hocs/PageLayoutHoc";
 import HomePage from "@/ui_components/home/HomePage";
 import withAuth from "@/hocs/withAuth";
 
-export default withAuth(PageLayoutHoc(HomePage, { showHeader: true }));
+export default withAuth(PageLayoutHoc(LiveChallenge, { showHeader: true }));
--- ui_components/challenges/LiveChallenge.jsx
+++ ui_components/challenges/LiveChallenge.jsx
@@ -2,71 +2,83 @@ import { BackBtn } from "../shared";
 import { ChallengeCard, LastSynced } from ".";
 import Image from "next/image";
 import { ICONS } from "@/utils/images";
+import { customXMTPMessagingService } from "@/contract/xmtpProtocol";
+import usePrivyClient from "@/utils/hooks/wallet/usePrivyClient";
+import { useEffect } from "react";
 import { useState } from "react";
 import { Chat } from "../messages";
 import UserChallengeDetail from "./UserChallengeDetail";
 
 const LiveChallenge = ({ challenge }) => {
+    // const { privyClient } = usePrivyClient();
+
+    // const fetchMessages = async () => {
+    //     console.log(privyClient, "privyClient");
+    //     if (privyClient && challenge.acceptedUser) {
+    //         const messages = await customXMTPMessagingService(
+    //             privyClient,
+    //         ).getConversation("0x06e70f295B6337c213DDe82D13cc198027687A7B");
+
+    //         console.log(messages, "messages");
+    //     }
+    // };
+
+    // const sendMessage = async () => {
+    //     if (privyClient && challenge.acceptedUser) {
+    //         const messages = await customXMTPMessagingService(
+    //             privyClient,
+    //         ).sendXMTPMessage(
+    //             "0x06e70f295B6337c213DDe82D13cc198027687A7B",
+    //             "Hi vaithi , complete it fast",
+    //         );
+
+    //         console.log(messages, "messages");
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     if (privyClient && challenge.acceptedUser) {
+    //         (async () => {
+    //             await fetchMessages();
+    //         })();
+    //     }
+    // }, [challenge?.acceptedUser]);
     const [currentTab, setCurrentTab] = useState(2);
-    const [showDetail, setShowDetail] = useState(false);
     const handleTabClick = (tabId) => {
         setCurrentTab(tabId);
-    };
-    const handleShowDetail = () => {
-        setShowDetail(!showDetail);
-    };
+    }
     return (
         <section className="relative h-[calc(100dvh-64px)] pt-4">
-            {!showDetail && (
-                <div>
-                    {currentTab === 1 ? (
-                        <div className="container mx-auto h-auto relative">
-                            <div className="h-full w-full">
-                                <BackBtn className="mb-2" />
-                                {/* <Button onClick={sendMessage}>Send Message</Button> */}
-                                <p className="paragraph_regular mb-1">Watch out!</p>
-                                <h2 className="heading2_bold mb-6">
-                                    Challenge is now live
-                                </h2>
-                                <ChallengeCard {...challenge} />
-                                <LastSynced />
-                                <div
-                                    onClick={() => {
-                                        handleShowDetail();
-                                    }}
-                                >
-                                    <div className="flex items-center justify-between mb-7">
-                                        <div className="flex items-center gap-1">
-                                            <Image
-                                                className="w-10 rounded-full"
-                                                src={ICONS.Profile}
-                                            />
-                                            <p className="paragraph_regular text-black">
-                                                You
-                                            </p>
-                                        </div>
-                                        <div className="flex items-center gap-2">
-                                            <p className="paragraph_regular text-red">
-                                                200 steps
-                                            </p>
-                                            <Image src={ICONS.chevronRight} />
-                                        </div>
-                                    </div>
-                                    <div className="flex items-center justify-between mb-7">
-                                        <div className="flex items-center gap-1">
-                                            <Image className="w-10" src={ICONS.Profile} />
-                                            <p className="paragraph_regular text-black">
-                                                You
-                                            </p>
-                                        </div>
-                                        <div className="flex items-center gap-2">
-                                            <p className="paragraph_regular text-green">
-                                                5000 steps
-                                            </p>
-                                            <Image src={ICONS.chevronRight} />
-                                        </div>
-                                    </div>
-                                </div>
+            {currentTab === 1 ? <div className="container mx-auto h-auto relative">
+                <div className="h-full w-full">
+                    <BackBtn className="mb-2" />
+                    {/* <Button onClick={sendMessage}>Send Message</Button> */}
+                    <p className="paragraph_regular mb-1">Watch out!</p>
+                    <h2 className="heading2_bold mb-6">Challenge is now live</h2>
+                    <ChallengeCard {...challenge} />
+                    <LastSynced />
+                    <div>
+                        <div className="flex items-center justify-between mb-7">
+                            <div className="flex items-center gap-1">
+                                <Image
+                                    className="w-10 rounded-full"
+                                    src={ICONS.Profile}
+                                />
+                                <p className="paragraph_regular text-black">You</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <p className="paragraph_regular text-red">200 steps</p>
+                                <Image src={ICONS.chevronRight} />
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-between mb-7">
+                            <div className="flex items-center gap-1">
+                                <Image className="w-10" src={ICONS.Profile} />
+                                <p className="paragraph_regular text-black">You</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <p className="paragraph_regular text-green">5000 steps</p>
+                                <Image src={ICONS.chevronRight} />
                             </div>
                         </div>
                     ) : (
@@ -109,6 +121,7 @@ const LiveChallenge = ({ challenge }) => {
                         )}
                     </div>
                 </div>
+<<<<<<< HEAD
             }
             {showDetail && (
                 <UserChallengeDetail
@@ -116,6 +129,17 @@ const LiveChallenge = ({ challenge }) => {
                     handleShowDetail={handleShowDetail}
                 />
             )}
+=======
+            </div> : <Chat/>}
+            <div className="bg-white grid grid-cols-2 fixed bottom-0 p-5 w-full md:w-[420px]">
+                <div className="flex justify-center items-center" onClick={() => handleTabClick(1)}>
+                    {currentTab === 1 ? <Image className="w-6 h-6" src={ICONS.barChartSel} /> : <Image className="w-6 h-6" src={ICONS.barChatUnsel} />}
+                </div>
+                <div className="flex justify-center items-center" onClick={() => handleTabClick(2)}>
+                    {currentTab === 2 ? <Image className="w-6 h-6" src={ICONS.chatSel} /> : <Image className="w-6 h-6" src={ICONS.chatUnsel} />}
+                </div>
+            </div>
+>>>>>>> c214431 (Added bottom nav for challenge page)
         </section>
     );
 };
